ee83f3e37056d6d483e4d21d65090f19ca0c11de
A601-00 <tnk.dat.class@gmail.com>
2022-06-09 14:03:57 +0900

    おまけ

--- fukuv0609/Form1.cs
+++ fukuv0609/Form1.cs
@@ -20,12 +20,38 @@ namespace fukuv0609
         private void label1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("田中");
+            timer1.Enabled = false;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += int.Parse(textBox1.Text);
             label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "-5";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-5";
+            textBox2.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "5";
+            textBox2.Text = "0";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "5";
         }
     }
 }
